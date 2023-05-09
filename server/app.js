6c4444dfa3d9ba5674d4c5278afff03a2c1e0e34
Mark Bavis <mark@mhack.io>
2023-05-08 22:25:17 -0700

    Remove the extra frontend redirection, just use app.js

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ const utils = require('utils/utils-express.js');
 const nocache = require('nocache');
 
 const sessionMiddleware = require('session.js');
-const frontendRouter = require('frontend.js');
 const setupSync = require('sync/sync.js');
+
+const setupAdminRoutes = require('users/admin.js');
+const setupLoginRoutes = require('users/login.js');
+const setupAccountRoutes = require('users/account.js');
 
 
 module.exports = async function(app, r)
@@ -30,11 +33,56 @@ module.exports = async function(app, r)
     // may pass info to frontend, so do that here
     await setupSync(app, r);
 
+    // get body
     r.use(
-        // frontend hookups
-        frontendRouter(app), 
+        express.urlencoded({extended:true}),
+    );
 
+    // make sure admin is first, so we always verify the admin user
+    setupAdminRoutes(app, r);
+    setupLoginRoutes(app, r);
+    setupAccountRoutes(app, r);
+
+    // page rendering
+    r.get(/^\/([^/]+).html$/,
+        (req, res) =>
+        {
+            const view = req.params[0];
+
+            const settings = req.app.ctx.settings;
+
+            res.initial_data.toasts = req.last_state && Array.isArray(req.last_state.toasts) ? req.last_state.toasts : [];
+
+            if (req.session.authed_user)
+            {
+                res.initial_data.authed_user = req.session.authed_user;
+            }
+
+            let params = { title:settings.title, ...res.template_params, initial_data: `<script>window.initial_data = ${JSON.stringify(res.initial_data)};</script>` };
+
+            if (!process.env.NO_MUSTACHE)
+            {
+                res.render(view, params,
+                    (err, html) =>
+                    {
+                        if (err)
+                        {
+                            res.status(404).end();
+                            console.log(err);
+                        }
+                        else
+                        {
+                            res.send(html).end();
+                        }
+                    });
+            }
+        }
+    );
+
+    r.use(
         // just in case
         (_, res) => res.status(404).end() 
     );
+
+    return r;
 };
